eacb74c8d5a357310af5b272078294881102e017
William E Hahn <williamedwardhahn@gmail.com>
2022-07-03 19:30:00 -0400

    Update parse_svg.py

--- HyperMapSVG/parse_svg.py
+++ HyperMapSVG/parse_svg.py
@@ -1,14 +1,13 @@
 import xml.etree.ElementTree as ET
 
-
 xml = ET.parse('lights.svg')
+
 root = xml.getroot()
 
+print(root.find('.//rect[@id="svg_1"]').get('style'))
 
 root.find('.//rect[@id="svg_1"]').set('style','display:none')
 
 xml.write('lights2.svg')
-
-
 
 
