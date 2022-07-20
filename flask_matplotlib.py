f5097816fdd41a6d01a8292bf6b25b1bd53290c7
William E Hahn <williamedwardhahn@gmail.com>
2022-07-20 18:40:41 -0400

    Create flask_matplotlib.py

---
+++ flask_matplotlib.py
@@ -0,0 +1,21 @@
+import base64
+from io import BytesIO
+
+from flask import Flask
+from matplotlib.figure import Figure
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
