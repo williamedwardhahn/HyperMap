acbd89e367be31a659a1ce323d80eabe2a55afda
William E Hahn <williamedwardhahn@gmail.com>
2023-05-10 11:37:04 -0400

    Update microdot_examples.py

--- microdot_examples.py
+++ microdot_examples.py
@@ -1,4 +1,49 @@
 ######################################################################
+#Coin Flip
+######################################################################
+
+from microdot_asyncio import Microdot, Response
+app = Microdot()
+Response.default_content_type = 'text/html'
+
+def htmldoc():
+    coin_text = "Heads" if coin_state == 0 else "Tails"
+
+    return f'''
+        <html>
+            <head>
+                <title>Click to Flip Coin</title>
+            </head>
+            <body>
+                <div>
+                    <h1>Click the Coin to Flip</h1>
+                    <svg width="200" height="200" viewBox="0 0 200 200">
+                        <a href="/toggle">
+                            <circle style="fill:#F0E68C" cx="100" cy="100" r="90"/>
+                            <text x="50%" y="50%" font-size="24" text-anchor="middle" dy=".3em">{coin_text}</text>
+                        </a>
+                    </svg>
+                </div>
+            </body>
+        </html>
+        '''
+
+coin_state = 0
+
+@app.route('/')
+def home(request):
+    return htmldoc()
+
+@app.route('/toggle')
+def toggle_coin(request):
+    global coin_state
+    coin_state = 1 - coin_state
+    return htmldoc()
+
+app.run(debug=True, port=8008)
+
+
+######################################################################
 #Guessing Game
 ######################################################################
 
